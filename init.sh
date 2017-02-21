1eb4dea0100593bec8162386bab6fed6177b1d20
diwa <543667537@qq.com>
2017-02-21 20:30:54 +0800

    add init

--- init.sh
+++ init.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
-
 #将jump.sh写入环境变量
-JUMPER_HOME=`pwd`
-export JUMPER_HOME
+export JUMPER_HOME=`pwd`
+echo $JUMPER_HOME
 
 #写入bash
 echo "export JUMPER_HOME=$JUMPER_HOME" >> ~/.bash_profile
-echo "alias jump=$JUMPER_HOME/jumper.sh" >> ~/.bash_profile
+echo "alias jump=\"$JUMPER_HOME/jump.sh\"" >> ~/.bash_profile
 
 #zsh
 echo "export JUMPER_HOME=$JUMPER_HOME" >> ~/.zshrc
-echo "alias jump=$JUMPER_HOME/jumper.sh" >> ~/.zshrc
+echo "alias jump=\"$JUMPER_HOME/jump.sh\"" >> ~/.zshrc
+
+#default setting
+cp env.setting.bak env.setting
+
+#chmod
+chmod +x $JUMPER_HOME/jump.sh
+chmod 777 $JUMPER_HOME/env.setting
+
 
