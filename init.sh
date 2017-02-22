75475d5225b993a0fecb8d1ca733c80c169615f2
diwa <543667537@qq.com>
2017-02-22 14:53:10 +0800

    also alias j

--- init.sh
+++ init.sh
@@ -1,15 +1,18 @@
 #将jump.sh写入环境变量
 export JUMPER_HOME=`pwd`
 echo $JUMPER_HOME
- alias jump=$JUMPER_HOME/jump.sh
+alias jump=$JUMPER_HOME/jump.sh
+alias j=$JUMPER_HOME/jump.sh
 
 #写入bash
 echo "export JUMPER_HOME=$JUMPER_HOME" >> ~/.bash_profile
 echo "alias jump=\"$JUMPER_HOME/jump.sh\"" >> ~/.bash_profile
+echo "alias j=\"$JUMPER_HOME/jump.sh\"" >> ~/.bash_profile
 
 #zsh
 echo "export JUMPER_HOME=$JUMPER_HOME" >> ~/.zshrc
 echo "alias jump=\"$JUMPER_HOME/jump.sh\"" >> ~/.zshrc
+echo "alias j=\"$JUMPER_HOME/jump.sh\"" >> ~/.zshrc
 
 #default setting
 cp env.setting.bak env.setting
